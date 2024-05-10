9bbada5df966aa06750f8e0057925bfa389e216a
adarog999 <adarog999@gmail.com>
2024-05-10 22:29:49 +0800

    server

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ io.sockets.on("connection",socket => {
         socket.broadcast.emit("broadcaster")
     });
     socket.on("watcher",() => {
-        socket.to(broadcaster).emit("watcher",socket.id)
+        io.to(broadcaster).emit("watcher",socket.id)
     });
     socket.on("disconnect",() => {
-        socket.to("broadcaster").emit("disconnectPeer",socket.id)
+        io.to("broadcaster").emit("disconnectPeer",socket.id)
     });
     socket.on("offer",(id,message) => {
-        socket.to(id).emit("offer",socket.id,message)
+        io.to(id).emit("offer",socket.id,message)
     });
     socket.on("answer",(id,message) => {
-        socket.to(id).emit("answer",socket.id,message)
+        io.to(id).emit("answer",socket.id,message)
     });
     socket.on("candidate",(id,message) => {
-        socket.to(id).emit("candidate",socket.id,message)
+        io.to(id).emit("candidate",socket.id,message)
     })
 })
 
